49a84f86da184ebec298648f689298cea4fa8fbd
Jimmy Lu <jimmy.lu.2011@gmail.com>
2012-02-17 11:14:10 -0500

    Defined WlppPlus Grammar!

--- src/parse_common.cc
+++ src/parse_common.cc
@@ -2,4 +2,19 @@
 
 namespace parse_common {
 
+Tree::Tree() {}
+
+Tree::~Tree()
+{
+    for (unsigned i = 0; i < this->leaves.size(); ++i)
+        delete this->leaves[i];
+}
+
+std::string Tree::getRule()
+{
+    switch (this->rule) {
+        default: throw "No such rule defined!\n"; break;
+    }
+}
+
 }
--- src/parse_common.h
+++ src/parse_common.h
@@ -13,24 +13,94 @@
 
 namespace parse_common {
 
+// The heart of parsing :-)
+//  this section defines the grammar of WlppPlus.
+//
+// Terminal symbols: the set of valid tokens as defined in common::Kind
+// Non-Terminal symbols: Start, BOF, EOF, procedures, procedure, type,
+//                       dcls, dcl, statements, statement, tests, test,
+//                       expr, term, term, lvalue.
+// Start symbol: Start
+// Production Rules: as defined below
 enum DerivationRule {
+    Start_Exp_Proc,         // Start -> BOF procedures EOF
+
+    Procs_Exp_ProcW_Procs,  // procedures -> procedureWain procedures
+    Procs_Exp_Procs_Proc,   // procedures -> procedures procedure
+    Procs_Exp_Nothing,      // procedures -> Nothing
+
+    ProcWain_Exp,           // procedureWain -> INTK WAIN LPAREN dcls RPAREN
+                            //                  LBRACE
+                            //                  dcls statements RETURN expr SEMI
+                            //                  RBRACE
+    Proc_Exp,               // procedure -> type ID LPAREN dcls RPAREN
+                            //              LBRACE
+                            //              dcls statements RETURN expr SEMI
+                            //              RBRACE
+
+    Type_Exp_Intk,          // type -> INTK
+    Type_Exp_IntkStar,      // type -> INTK STAR
+    Type_Exp_Chark,         // type -> CHARK
+    Type_Exp_CharkStar,     // type -> CHARK STAR
+
+    Dcls_Exp_Assign,        // dcls -> dcls dcl BECOMES expr SEMI
+    Dcls_Exp_NoAssign,      // dcls -> dcls dcl SEMI
+    Dcls_Exp_Nothing,       // dcls -> Nothing
+    Dcl_Exp,                // dcl -> type ID
+
+    Stmnts_Exp_Stmnts_Stmnt,// statements -> statements statement
+    Stmnts_Exp_Nothing,     // statements -> NULL
+    Stmnt_Exp_Assign,       // statements -> lvalue BECOMES expr SEMI
+    Stmnt_Exp_If,           // statements -> IF LPAREN tests AND test RPAREN
+                            //               LBRACE statements RBRACE
+                            //               ELSE LBRACE statements RBRACE
+    Stmnt_Exp_While,        // statements -> WHILE LPAREN tests AND test RPAREN
+                            //               LBRACE statements RBRACE
+
+    Tests_Exp_Tests_Test,   // tests -> tests and test
+    Tests_Exp_Nothing,      // tests -> Nothing
+
+    Test_Exp_Eq,            // test -> expr EQ expr
+    Test_Exp_NE,            // test -> expr NE expr
+    Test_Exp_LT,            // test -> expr LT expr
+    Test_Exp_LE,            // test -> expr LE expr
+    Test_Exp_GE,            // test -> expr GE expr
+    Test_Exp_GT,            // test -> expr GT expr
+
+    Expr_Exp_Term,          // expr -> term
+    Expr_Exp_Plus,          // expr -> expr PLUS term
+    Expr_Exp_Minus,         // expr -> expr MINUS term
+
+    Term_Exp_Ftor,          // term -> factor
+    Term_Exp_Star,          // term -> term STAR factor
+    Term_Exp_Slash,         // term -> term SLASH factor
+    Term_Exp_Pct,           // term -> term PCT factor
+
+    Ftor_Exp_Id,            // factor -> ID
+    Ftor_Exp_Int,           // factor -> INT
+    Ftor_Exp_Char,          // factor -> CHAR
+    Ftor_Exp_Null,          // factor -> NULL
+    Ftor_Exp_Expr,          // factor -> LPAREN expr RPAREN
+    Ftor_Exp_Addr,          // factor -> AMP lvalue
+    Ftor_Exp_Ptr,           // factor -> STAR factor
+
+    Lval_Exp_Id,            // lvalue -> ID
+    Lval_Exp_Ptr,           // lvalue -> STAR factor
+    Lval_Exp_Lval,          // lvalue -> LPAREN lvalue RPAREN
 };
 
-class tree {
+class Tree {
     public:
-        tree();
-        ~tree();
+        Tree();
+        ~Tree();
         std::string getRule();
-        tree* getLeaf(int i);
+        Tree* getLeaf(int i);
         std::string getID();    // Only terminal symbols have identifiers
 
     private:
         DerivationRule rule;
         std::vector<std::string> units;
-        std::vector<tree*> leaves;
-};
-
-class treePtr {
+        std::vector<Tree*> leaves;
 };
 
 }
